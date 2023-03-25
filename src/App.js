2e02a05d8ecfc6c0ba8415a4b78df456ba85107c
Warghto <vasilijtsaryk@gmail.com>
2023-03-25 16:16:38 +0100

    Header and content part with images

--- src/App.js
+++ src/App.js
@@ -1,31 +1,95 @@
-import "./styles.css";
+import "./index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <div className="headerLeft">
-          <svg />
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
-            <p>Магазин лучших кроссовок</p>
+      <div className="App clear">
+        <header className="d-flex justify-between align-center p-40">
+          <div className="d-flex align-center">
+            <img width={40} height={40}src ="img/logo.png"/>
+            <div>
+              <h3 className="text-uppercase">React Sneakers</h3>
+              <p className="opacity-5">Best Sneakers-shop EUW</p>
+            </div>
           </div>
+          <ul className="d-flex">
+            <li className="mr-30">
+              <img width={18} height={18}src ="img/cart.svg"/>
+              <span>1205 eur.</span>
+            </li>
+            <li>
+              <img width={18} height={18}src ="img/user.svg"/>
+            </li>
+          </ul>
+        </header>
+        <div className="content p-40">
+          <h1 className="mb-40">All Sneakers</h1>
+          <div className="d-flex">
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>399 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11}height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+
+            </div>
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers" />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>399 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11}height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+
+            </div>
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers" />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>399 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11}height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+
+            </div>
+            <div className="card">
+              <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers" />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>399 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11}height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+
+            </div>
+
+          </div>
+
+
         </div>
-        <ul className="headerRight">
-          <li>
-            <svg />
-            <span>1205 eur.</span>
-          </li>
-          <li>
-            <svg />
-          </li>
-        </ul>
-      </header>
-      <div className="content">
-        <h1>Все кроссовки</h1>
       </div>
-    </div>
   );
 }
+
+
+
 export default App;
 //шапка, карточки товаров,
