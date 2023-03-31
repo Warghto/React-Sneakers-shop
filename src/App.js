7f60c4126c4ce3da8a9c423eebc046def5110d2d
Warghto <vasilijtsaryk@gmail.com>
2023-03-31 19:39:01 +0200

    cart window updated

--- src/App.js
+++ src/App.js
@@ -3,93 +3,239 @@ import "./index.scss";
 function App() {
   return (
       <div className="App clear">
+        <div className="overlay">
+          <div className="drawer">
+            <h2 className="mb-20">Shopping cart</h2>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                  style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                  className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazer Mid Suede</p>
+                <b>199 eur.</b>
+              </div>
+              <svg
+                  className="removeBtn"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#DBDBDB"
+                />
+                <path
+                    d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
+                    fill="#B5B5B5"
+                />
+              </svg>
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                  style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
+                  className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazer Mid Suede</p>
+                <b>199 eur.</b>
+              </div>
+              <svg
+                  className="removeBtn"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                    x="0.5"
+                    y="0.5"
+                    width="31"
+                    height="31"
+                    rx="7.5"
+                    fill="white"
+                    stroke="#DBDBDB"
+                />
+                <path
+                    d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
+                    fill="#B5B5B5"
+                />
+              </svg>
+            </div>
+            <div className="items">
+
+              <ul className="cartTotalBlock">
+                <li>
+                  <span>Total price:</span>
+                  <div></div>
+                  <b> 398 eur.</b>
+                </li>
+
+                <li>
+                  <span>Fee 5%:</span>
+                  <div></div>
+                  <b> 7 eur.</b>
+                </li>
+              </ul>
+
+
+            </div>
+
+          </div>
+        </div>
         <header className="d-flex justify-between align-center p-40">
           <div className="d-flex align-center">
-            <img width={40} height={40}src ="img/logo.png"/>
+            <img width={40} height={40} src="img/logo.png" />
             <div>
-              <h3 className="text-uppercase">React Sneakers</h3>
+              <h3 className="text-uppercase">Sneaker Spot</h3>
               <p className="opacity-5">Best Sneakers-shop EUW</p>
             </div>
           </div>
           <ul className="d-flex">
             <li className="mr-30">
-              <img width={18} height={18}src ="img/cart.svg"/>
+              <img width={18} height={18} src="img/cart.svg" />
               <span>1205 eur.</span>
             </li>
             <li>
-              <img width={18} height={18}src ="img/user.svg"/>
+              <img width={18} height={18} src="img/user.svg" />
             </li>
           </ul>
         </header>
         <div className="content p-40">
-          <h1 className="mb-40">All Sneakers</h1>
-          <div className="d-flex">
-            <div className="card">
-              <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-              <h5>Nike Blazer Mid Suede</h5>
-              <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column ">
-                  <span>Price:</span>
-                  <b>199 eur.</b>
-                </div>
-                <button className="button">
-                  <img width={11}height={11} src="/img/plus.svg" />
-                </button>
-              </div>
-
+          <div className="d-flex align-center justify-between mb-40">
+            <h1>All Sneakers</h1>
+            <div className="Search-block d-flex">
+              <svg
+                  width="20"
+                  height="40"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                    d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z"
+                    stroke="#E4E4E4"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                />
+              </svg>
+              <input placeholder="Search..." />
             </div>
-            <div className="card">
-              <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers" />
-              <h5>Nike Blazer Mid Suede</h5>
-              <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column ">
-                  <span>Price:</span>
-                  <b>199 eur.</b>
-                </div>
-                <button className="button">
-                  <img width={11}height={11} src="/img/plus.svg" />
-                </button>
-              </div>
-
-            </div>
-            <div className="card">
-              <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers" />
-              <h5>Nike Blazer Mid Suede</h5>
-              <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column ">
-                  <span>Price:</span>
-                  <b>199 eur.</b>
-                </div>
-                <button className="button">
-                  <img width={11}height={11} src="/img/plus.svg" />
-                </button>
-              </div>
-
-            </div>
-            <div className="card">
-              <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers" />
-              <h5>Nike Blazer Mid Suede</h5>
-              <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column ">
-                  <span>Price:</span>
-                  <b>199 eur.</b>
-                </div>
-                <button className="button">
-                  <img width={11}height={11} src="/img/plus.svg" />
-                </button>
-              </div>
-
-            </div>
-
           </div>
 
-
+          <div className="d-flex">
+            <div className="card">
+              <div className="favorite">
+                <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                      x="0.5"
+                      y="0.5"
+                      width="31"
+                      height="31"
+                      rx="6.5"
+                      fill="white"
+                      stroke="#F8F8F8"
+                  />
+                  <path
+                      d="M21.149 11.356L21.1484 11.3554C20.8095 11.0258 20.4097 10.7636 19.9705 10.5833L19.9696 10.5829C19.5139 10.3951 19.0249 10.2989 18.5311 10.3L18.5295 10.3C17.8346 10.3 17.1584 10.4887 16.5717 10.8438L16.5717 10.8439C16.4313 10.9288 16.2985 11.0218 16.173 11.1228L15.7341 11.476L15.2953 11.1228C15.1698 11.0218 15.037 10.9288 14.8966 10.8439L14.8966 10.8438C14.3099 10.4887 13.6337 10.3 12.9388 10.3C12.4373 10.3 11.9546 10.395 11.4987 10.5829L11.498 10.5832C11.057 10.7644 10.6606 11.0243 10.3197 11.3556L10.3187 11.3566L10.3187 11.3566C9.98111 11.6834 9.71174 12.0725 9.52557 12.5016L21.149 11.356ZM21.149 11.356C21.4865 11.6835 21.7561 12.0729 21.943 12.5022C22.1365 12.9487 22.2347 13.4204 22.2333 13.9064V13.9084C22.2333 14.3625 22.1399 14.8512 21.9418 15.3651L21.9412 15.3667M21.149 11.356L21.9412 15.3667M12.3799 19.1131L12.38 19.1132C13.1291 19.7996 13.8773 20.3822 14.4475 20.7988C14.732 21.0066 14.9709 21.1721 15.1415 21.2873C15.2268 21.345 15.2948 21.3899 15.3428 21.4212C15.3857 21.4492 15.4091 21.464 15.4144 21.4673C15.4154 21.4679 15.4158 21.4682 15.4155 21.468L15.4266 21.4748L15.4266 21.4749L15.7333 21.6701L16.0401 21.4749L16.0402 21.4748C16.0914 21.4423 17.5822 20.4902 19.0868 19.1131H12.3799ZM12.3799 19.1131C11.4753 18.2845 10.7634 17.4818 10.255 16.7299M12.3799 19.1131L10.255 16.7299M21.9412 15.3667C21.7771 15.7954 21.5328 16.2542 21.2114 16.7303M21.9412 15.3667L21.2114 16.7303M10.255 16.7299C9.93467 16.2553 9.69124 15.796 9.52486 15.3651L10.255 16.7299ZM21.2114 16.7303C20.7031 17.482 19.9913 18.2845 19.087 19.1129L21.2114 16.7303ZM9.23333 13.9084C9.23333 13.4208 9.33184 12.9483 9.52554 12.5017L9.52472 15.3648C9.32672 14.851 9.23333 14.3624 9.23333 13.9084Z"
+                      stroke="#ECECEC"
+                      stroke-width="1.4"
+                  />
+                </svg>
+              </div>
+              <img
+                  width={133}
+                  height={112}
+                  src="/img/sneakers/1.jpg"
+                  alt="Sneakers"
+              />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>199 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <img
+                  width={133}
+                  height={112}
+                  src="/img/sneakers/2.jpg"
+                  alt="Sneakers"
+              />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>199 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <img
+                  width={133}
+                  height={112}
+                  src="/img/sneakers/3.jpg"
+                  alt="Sneakers"
+              />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>199 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <img
+                  width={133}
+                  height={112}
+                  src="/img/sneakers/4.jpg"
+                  alt="Sneakers"
+              />
+              <h5>Nike Blazer Mid Suede</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                  <span>Price:</span>
+                  <b>199 eur.</b>
+                </div>
+                <button className="button">
+                  <img width={11} height={11} src="/img/plus.svg" />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
   );
 }
-
-
 
 export default App;
 //шапка, карточки товаров,
