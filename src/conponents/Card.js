4fc21a23fa3e2c825b1772cdc93afcb76940ca1a
Warghto <vasilijtsaryk@gmail.com>
2023-04-04 11:47:34 +0200

    added components with map

--- src/conponents/Card.js
+++ src/conponents/Card.js
@@ -1,26 +1,22 @@
-function Card(){
-    return(
+function Card(props) {
+    console.log(props.title);
+    return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/unliked.svg" alt="unliked" />
             </div>
-            <img
-                width={133}
-                height={112}
-                src="/img/sneakers/1.jpg"
-                alt="Sneakers"
-            />
-            <h5>Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column ">
                     <span>Price:</span>
-                    <b>199 eur.</b>
+                    <b>{props.price} eur.</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plus.svg" />
                 </button>
             </div>
         </div>
-    )
+    );
 }
 export default Card;
