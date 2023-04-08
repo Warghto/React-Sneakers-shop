7d4ea1951701ca360c92383e39c65fdee0b53bfc
Warghto <vasilijtsaryk@gmail.com>
2023-04-08 22:54:05 +0200

    Update index.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Card.module.scss";
-function Card({ onFavorite,  title, imageUrl, price}) {
+function Card({ onFavorite,  title, imageUrl, price, onPlus}) {
     const [isAdded, setIsAdded] = React.useState(false);
 
     const onClickPlus = () => {
+        onPlus();
         setIsAdded(!isAdded);
     };
 
