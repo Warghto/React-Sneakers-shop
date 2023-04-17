82dafb4b3833495656ba26429a65d25665b70177
Avoid <vasilijtsaryk@gmail.com>
2023-04-17 04:33:29 +0000

    need to fix mockapi with added products and correctly use heart.svg to add products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-function Header(props){
-    return(
-
-        <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-            <img width={40} height={40} src="img/logo.png" alt="Logo"/>
-            <div>
-                <h3 className="text-uppercase">Sneaker Spot</h3>
-                <p className="opacity-5">Best Sneakers-shop EUW</p>
-            </div>
+function Header(props) {
+  return (
+    <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} src="img/logo.png" alt="Logo" />
+        <div>
+          <h3 className="text-uppercase">Sneaker Spot</h3>
+          <p className="opacity-5">Best Sneakers-shop EUW</p>
         </div>
-        <ul className="d-flex">
-            <li onClick={props.onClickCart} className="mr-30 cu-p">
-                <img width={18} height={18} src="img/cart.svg" />
-                <span>1205 eur.</span>
-            </li>
-            <li>
-                <img width={18} height={18} src="img/user.svg" />
-            </li>
-        </ul>
-
-
+      </div>
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-30 cu-p">
+          <img width={18} height={18} src="img/cart.svg" alt="trashcan" />
+          <span>1205 eur.</span>
+        </li>
+        <li>
+          <img width={18} height={18} src="img/heart.svg" alt="favorites" />
+        </li>
+        <li>
+          <img width={18} height={18} src="img/user.svg" alt="user" />
+        </li>
+      </ul>
     </header>
-    );
+  );
 }
 export default Header;
