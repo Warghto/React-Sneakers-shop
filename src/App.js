53334f95a393c5b70b0f8000c95a3747f95d98f1
Avoid <vasilijtsaryk@gmail.com>
2023-04-24 08:26:36 +0000

    added pre-download cards and fake array && react.skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import axios from "axios";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
 import "./index.scss";
-
 
 function App() {
   const [items, setItems] = React.useState([]);
@@ -15,49 +14,64 @@ function App() {
   const [favorites, setFavorites] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-   async function fetchData() {
+    async function fetchData() {
+      const cartResponse = await axios.get(
+        "https://643062f7b289b1dec4c76583.mockapi.io/cart"
+      );
+      const favoritesResponse = await axios.get(
+        "https://643806eac1565cdd4d6435e6.mockapi.io/favorites"
+      );
+      const itemsResponse = await axios.get(
+        "https://643062f7b289b1dec4c76583.mockapi.io/items"
+      );
 
-     const cartResponse = await axios.get("https://643062f7b289b1dec4c76583.mockapi.io/cart");
-     const  favoritesResponse = await axios.get("https://643806eac1565cdd4d6435e6.mockapi.io/favorites");
-     const  itemsResponse =  await axios.get("https://643062f7b289b1dec4c76583.mockapi.io/items");
+      setIsLoading(false);
 
-
-     setCartItems(cartResponse.data);
-     setFavorites(favoritesResponse.data);
-     setItems(itemsResponse.data);
-
-   }
+      setCartItems(cartResponse.data);
+      setFavorites(favoritesResponse.data);
+      setItems(itemsResponse.data);
+    }
     fetchData();
   }, []);
 
   const onAddToCart = (obj) => {
-    try{
-      if(cartItems.find((item) => Number(item.id) === Number(obj.id))){
-        axios.delete(`https://643062f7b289b1dec4c76583.mockapi.io/cart/${obj.id}`)
-        setCartItems(prev => prev.filter(item  => Number(item.id) !== Number(obj.id) ));
-      }else{
-        axios.post('https://643062f7b289b1dec4c76583.mockapi.io/cart', obj);
+    try {
+      if (cartItems.find((item) => Number(item.id) === Number(obj.id))) {
+        axios.delete(
+          `https://643062f7b289b1dec4c76583.mockapi.io/cart/${obj.id}`
+        );
+        setCartItems((prev) =>
+          prev.filter((item) => Number(item.id) !== Number(obj.id))
+        );
+      } else {
+        axios.post("https://643062f7b289b1dec4c76583.mockapi.io/cart", obj);
         setCartItems((prev) => [...prev, obj]);
       }
-    }catch (error){
-      alert('Error with products in cart')
+    } catch (error) {
+      alert("Error with products in cart");
     }
   };
 
   const onAddToFavorite = async (obj) => {
-  try{
-    if(favorites.find((favObj) => favObj.id === obj.id)){
-      axios.delete(`https://643806eac1565cdd4d6435e6.mockapi.io/favorites/${obj.id}`)
-      setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
-    }else{
-      const {data} = await axios.post('https://643806eac1565cdd4d6435e6.mockapi.io/favorites', obj);
-      setFavorites((prev) => [...prev, data]);
+    try {
+      if (favorites.find((favObj) => favObj.id === obj.id)) {
+        axios.delete(
+          `https://643806eac1565cdd4d6435e6.mockapi.io/favorites/${obj.id}`
+        );
+        setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
+      } else {
+        const { data } = await axios.post(
+          "https://643806eac1565cdd4d6435e6.mockapi.io/favorites",
+          obj
+        );
+        setFavorites((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert("Error, can`t add to favorites");
     }
-  }catch (error){
-    alert('Error, can`t add to favorites')
-  }
   };
 
   const onRemoveItem = (id) => {
@@ -70,7 +84,6 @@ function App() {
   };
 
   return (
-
     <div className="App clear">
       {cartOpened && (
         <Drawer
@@ -82,22 +95,29 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
 
       <Routes>
-        <Route path="/" element={<Home
-            items={items}
-            cartItems={cartItems}
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            onChangeSearchInput={onChangeSearchInput}
-            onAddToFavorite={onAddToFavorite}
-            onAddToCart={onAddToCart}/>} />
-        <Route path="/favorites" element={<Favorites
-            items={favorites}
-            onAddToFavorite={onAddToFavorite}
-        />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              items={items}
+              cartItems={cartItems}
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              onChangeSearchInput={onChangeSearchInput}
+              onAddToFavorite={onAddToFavorite}
+              onAddToCart={onAddToCart}
+              isLoading={isLoading}
+            />
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
+          }
+        />
       </Routes>
-
     </div>
-
   );
 }
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,40 +1,71 @@
 import React from "react";
+
 import styles from "./Card.module.scss";
-function Card({ id, onFavorite,  title, imageUrl, price, onPlus, favorited = false, added = false}) {
-    const [isAdded, setIsAdded] = React.useState(added);
-    const [isFavorite, setIsFavorite] = React.useState(favorited);
+import ContentLoader from "react-content-loader";
 
-    const onClickPlus = () => {
-        onPlus({id, title, imageUrl, price});
-        setIsAdded(!isAdded);
-    };
+function Card({
+  id,
+  onFavorite,
+  title,
+  imageUrl,
+  price,
+  onPlus,
+  favorited = false,
+  added = false,
+  loading = false,
+}) {
+  const [isAdded, setIsAdded] = React.useState(added);
+  const [isFavorite, setIsFavorite] = React.useState(favorited);
 
-    const onClickFavorite = () => {
-        onFavorite({id,title, imageUrl, price});
-        setIsFavorite(!isFavorite)
-    }
+  const onClickPlus = () => {
+    onPlus({ id, title, imageUrl, price });
+    setIsAdded(!isAdded);
+  };
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.favorite} onClick={onClickFavorite}>
-                <img src={isFavorite ? "/img/liked.svg" : "/img/unliked.svg"} />
+  const onClickFavorite = () => {
+    onFavorite({ id, title, imageUrl, price });
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <div className={styles.card}>
+      {loading ? (
+        <ContentLoader
+          speed={2}
+          width={160}
+          height={265}
+          viewBox="0 0 160 265"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="1" y="0" rx="10" ry="10" width="160" height="155" />
+          <rect x="0" y="167" rx="5" ry="5" width="160" height="15" />
+          <rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
+          <rect x="1" y="234" rx="5" ry="5" width="80" height="25" />
+          <rect x="118" y="230" rx="10" ry="10" width="32" height="32" />
+        </ContentLoader>
+      ) : (
+        <>
+          <div className={styles.favorite} onClick={onClickFavorite}>
+            <img src={isFavorite ? "/img/liked.svg" : "/img/unliked.svg"} />
+          </div>
+          <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+          <h5>{title}</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column ">
+              <span>Price:</span>
+              <b>{price} eur.</b>
             </div>
-            <img width={133} height={112} src={imageUrl} alt="Sneakers" />
-            <h5>{title}</h5>
-            <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column ">
-                    <span>Price:</span>
-                    <b>{price} eur.</b>
-                </div>
-                <img
-                    className={styles.plus}
-                    onClick={onClickPlus}
-                    src={isAdded ? "/img/btn-checked.svg" : "/img/plus.svg"}
-                    alt="Plus"
-                />
-            </div>
-        </div>
-    );
+            <img
+              className={styles.plus}
+              onClick={onClickPlus}
+              src={isAdded ? "/img/btn-checked.svg" : "/img/plus.svg"}
+              alt="Plus"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 export default Card;
-
